6da705da9a49219e61e29a563303a82f4ca7635a
Vegard Nossum <vegard.nossum@oracle.com>
2016-07-14 23:21:35 -0400

    next3: short-cut orphan cleanup on error
    [amir:] backport of upstream ext4 commit c65d5c6c81a1f for next3
    
    If we encounter a filesystem error during orphan cleanup, we should stop.
    Otherwise, we may end up in an infinite loop where the same inode is
    processed again and again.
    
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/next3/super.c
+++ fs/next3/super.c
@@ -1662,6 +1662,16 @@ static void next3_orphan_cleanup (struct super_block * sb,
 	while (es->s_last_orphan) {
 		struct inode *inode;
 
+		/*
+		 * We may have encountered an error during cleanup; if
+		 * so, skip the rest.
+		 */
+		if (NEXT3_SB(sb)->s_mount_state & NEXT3_ERROR_FS) {
+			jbd_debug(1, "Skipping orphan recovery on fs with errors.\n");
+			es->s_last_orphan = 0;
+			break;
+		}
+
 		inode = next3_orphan_get(sb, le32_to_cpu(es->s_last_orphan));
 		if (IS_ERR(inode)) {
 			es->s_last_orphan = 0;
