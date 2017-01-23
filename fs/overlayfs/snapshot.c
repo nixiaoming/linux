f64b30959c46e79df72f92fb9b11054377df24ed
Amir Goldstein <amir73il@gmail.com>
2017-01-23 13:33:24 +0200

    ovl: support freeze/thaw of snapshot mount
    Call super_freeze()/super_thaw() for upper fs to freeze
    the snapshot mount and block writers.
    
    This is going to be used for snapshot take while there are
    files open for write.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/snapshot.c
+++ fs/overlayfs/snapshot.c
@@ -206,6 +206,20 @@ static const struct dentry_operations ovl_snapshot_dentry_operations = {
 	.d_revalidate = ovl_snapshot_revalidate,
 };
 
+static int ovl_snapshot_freeze(struct super_block *sb)
+{
+	struct ovl_fs *ofs = sb->s_fs_info;
+
+	return freeze_super(ofs->upper_mnt->mnt_sb);
+}
+
+static int ovl_snapshot_unfreeze(struct super_block *sb)
+{
+	struct ovl_fs *ofs = sb->s_fs_info;
+
+	return thaw_super(ofs->upper_mnt->mnt_sb);
+}
+
 static int ovl_snapshot_show_options(struct seq_file *m, struct dentry *dentry)
 {
 	struct super_block *sb = dentry->d_sb;
@@ -230,6 +244,8 @@ static const struct super_operations ovl_snapshot_super_operations = {
 	.drop_inode	= generic_delete_inode,
 	.put_super	= ovl_put_super,
 	.sync_fs	= ovl_sync_fs,
+	.freeze_fs	= ovl_snapshot_freeze,
+	.unfreeze_fs	= ovl_snapshot_unfreeze,
 	.statfs		= ovl_statfs,
 	.show_options	= ovl_snapshot_show_options,
 	.remount_fs	= ovl_snapshot_remount,
