f4746aa20a956938057d8faff1e8ca8fdf739b39
Amir Goldstein <amir73il@gmail.com>
2017-09-20 09:09:30 +0300

    ovl: create orphan index on open for read
    With consistent_fd feature enabled, after copy up on open for read,
    upperdentry remains negative and index is created as orphan (no nlink
    xattr and i_nlink 1). On first open for write, upperdentry is linked to
    the orphan index which is then no longer orphan.
    
    After force reboot, file system will cleanup all orphan index entries
    that were created on open for read and were never opened for write.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/util.c
+++ fs/overlayfs/util.c
@@ -540,6 +540,10 @@ bool ovl_need_index(struct dentry *dentry)
 	    !d_is_dir(lower) && d_inode(lower)->i_nlink > 1)
 		return true;
 
+	/* Index all regular files if consistent fd is enabled */
+	if (ofs->config.consistent_fd && d_is_reg(lower))
+		return true;
+
 	return false;
 }
 
