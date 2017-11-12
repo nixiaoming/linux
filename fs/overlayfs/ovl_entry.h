2fdf80cb4bfaabea475f1a9b71babc44319b4594
Amir Goldstein <amir73il@gmail.com>
2017-11-12 17:50:49 +0200

    ovl: prepare for changing snapshot overlay
    Drop the assumption that ofs->snapshot_mnt and the snapshot overlay
    dentries oe->__snapdentry are not changing throughout the life time
    of the snapshot mount:
    - Rename the ovl_fs struct field to __snapmnt
    - Access __snapmnt and __snapdentry using rcu_dereference()
    - Use helpers ovl_snapshot_path() and ovl_snapshot_dentry()
      to get __snapdentry and __snapmnt with elevated refcount
    - Never access those pointers for non snapshot mount case
    
    ovl_snapshot_path() returns error for the following cases:
    ESTALE - __snapdentry is not from __snapmnt
    ENOENT - __snapdentry is NULL and __snapmnt is not NULL
    
    ESTALE implies that __snapdentry should be looked up again after
    __snapmnt has changed.
    
    ENOENT implies that __snapdentry should be looked up after
    __snapmnt was set for the first time.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/ovl_entry.h
+++ fs/overlayfs/ovl_entry.h
@@ -38,7 +38,7 @@ struct ovl_path {
 /* private information held for overlayfs's superblock */
 struct ovl_fs {
 	struct vfsmount *upper_mnt;
-	struct vfsmount *snapshot_mnt;
+	struct vfsmount *__snapmnt;
 	unsigned numlower;
 	struct ovl_layer *lower_layers;
 	/* workbasedir is the path at workdir= mount option */
