3a11eb40f8dbdb29545f19aff8539734f5578632
Amir Goldstein <amir73il@gmail.com>
2017-11-21 22:11:11 +0200

    ovl: set redirect xattr for lower followed by origin fh
    With mount option "redirect_dir=origin", if upper dir has a stored
    origin fh xattr and that file handle points to a lower directory that
    was renamed, the upper dir becomes implicitly redirected.
    
    Decoding a directory file handle can be expensive, because it involves
    reading directories up to root of the super block and trying to connect
    the decoded dentry.
    
    When a renamed origin is detected, set the redirect xattr on upper dir
    to the path of the decoded origin, so future lookups will find the
    correct lower.
    
    If there are lower layers below the decoded origin, return -ESTALE after
    setting upper redirect xattr to repeat lookup again by redirect and
    continue to lower layers lookup.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/overlayfs/dir.c
+++ fs/overlayfs/dir.c
@@ -882,7 +882,15 @@ static bool ovl_can_move(struct dentry *dentry)
 		!d_is_dir(dentry) || !ovl_type_merge_or_lower(dentry);
 }
 
-static char *ovl_get_redirect(struct dentry *dentry, bool abs_redirect)
+/*
+ * This routine provides functionality similar to realpath(2), where upper
+ * redirects along the path are expanded while resolving path components.
+ * When @nofollow is true, redirects are not exapnaded. @nofollow can be used
+ * to get the upper redirect path from the underlying lower path.
+ * If @root is non NULL, get the path relative to @root dentry.
+ */
+char *ovl_get_redirect(struct dentry *dentry, bool abs_redirect, bool nofollow,
+		       struct dentry *root)
 {
 	char *buf, *ret;
 	struct dentry *d, *tmp;
@@ -900,12 +908,13 @@ static char *ovl_get_redirect(struct dentry *dentry, bool abs_redirect)
 
 	buflen--;
 	buf[buflen] = '\0';
-	for (d = dget(dentry); !IS_ROOT(d);) {
-		const char *name;
+	for (d = dget(dentry); !IS_ROOT(d) && d != root;) {
+		const char *name = NULL;
 		int thislen;
 
 		spin_lock(&d->d_lock);
-		name = ovl_dentry_get_redirect(d);
+		if (!nofollow)
+			name = ovl_dentry_get_redirect(d);
 		if (name) {
 			thislen = strlen(name);
 		} else {
@@ -929,7 +938,7 @@ static char *ovl_get_redirect(struct dentry *dentry, bool abs_redirect)
 		d = tmp;
 
 		/* Absolute redirect: finished */
-		if (buf[buflen] == '/')
+		if (!nofollow && buf[buflen] == '/')
 			break;
 		buflen--;
 		buf[buflen] = '/';
@@ -978,7 +987,7 @@ static int ovl_set_redirect(struct dentry *dentry, bool samedir)
 	if (redirect && (!absolute_redirect || redirect[0] == '/'))
 		return 0;
 
-	redirect = ovl_get_redirect(dentry, absolute_redirect);
+	redirect = ovl_get_redirect(dentry, absolute_redirect, false, NULL);
 	if (IS_ERR(redirect))
 		return PTR_ERR(redirect);
 
